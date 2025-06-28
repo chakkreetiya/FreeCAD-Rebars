ab04af893489740b551b47038af09af36e982564
chakkreetiya <chakkree@hotmail.com>
2025-06-28 15:35:45 +0700

    Create setup.py
    for setup FreeCAD Workbench

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+import sys, os
+
+version_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 
+                            "freecad", "chakkree_rebar_tools", "version.py")
+
+with open(version_path) as fp:
+    exec(fp.read())
+
+setup(name='freecad.chakkree_rebar_tools',
+      version=str(__version__),
+      packages=['freecad',
+                'freecad.chakkree_rebar_tools'],
+      maintainer="chakkree tiyawongsuwan",
+      #maintainer_email="eng.maykowmenezes@gmail.com",
+      #url="https://www.patreon.com/c/StructureTools",
+      description="Workbench for rebar in concrete structure.",
+      #install_requires=['numpy','scipy'],
+      include_package_data=True)
